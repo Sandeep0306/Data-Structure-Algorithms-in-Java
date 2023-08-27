571b80706f80beaf0e30cea01b9421b9361ad810
Sandeep Biswal <112690853+Sandeep0306@users.noreply.github.com>
2023-08-27 16:57:40 +0530

    Time: 51 ms (59.71%), Space: 48.6 MB (72.56%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canCross(int[] stones) {
+        Map<Integer,HashSet<Integer>> map = new HashMap<>();
+
+        for(int stone:stones){
+            map.put(stone,new HashSet<>());
+        }
+        map.get(stones[0]).add(1);
+        for(int i=0; i<stones.length;i++){
+            int curStone = stones[i];
+            HashSet<Integer> jmp = map.get(curStone);
+            for(int jump:jmp){
+                int pos = curStone +jump;
+
+                if(pos== stones[stones.length-1]) return true;
+                if(map.containsKey(pos)){
+                    if(jump-1 >0){
+                        map.get(pos).add(jump-1);
+                    }
+                    map.get(pos).add(jump);
+                    map.get(pos).add(jump+1);
+                }
+            }
+        }
+
+        return false;
+    }
+}
